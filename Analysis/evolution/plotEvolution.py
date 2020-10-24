83cff83398dd410ec5af91a2bc594c1d7d0093c7
jbirky <jbirky@uw.edu>
2020-10-23 17:11:19 -0700

    plot evolution

---
+++ Analysis/evolution/plotEvolution.py
@@ -0,0 +1,201 @@
+import numpy as np
+import vplot as vpl
+import os
+import glob
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+from matplotlib import rc
+plt.style.use('classic')
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+rc('figure', facecolor='w')
+rc('xtick', labelsize=20)
+rc('ytick', labelsize=20)
+
+import sys
+sys.path.append('../../Scripts/Rup147_CTL')
+import config
+from config import kwargs
+from config import m1, m1Sig, m2, m2Sig, \
+				   r1, r1Sig, r2, r2Sig, \
+				   teff1, teff1Sig, teff2, teff2Sig, \
+				   lum1, lum1Sig, lum2, lum2Sig, \
+				   prot1, prot1Sig, porb, porbSig, \
+				   ecc, eccSig, age, ageSig
+
+plotName = 'evolution.png'
+
+# ===================================================
+# Load sim files
+# ===================================================
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+OUTPATH = 'sims'
+
+kwargs['PATH'] = PATH
+kwargs['OUTPATH'] = OUTPATH
+
+# remove all old files from OUTPATH
+files = glob.glob(os.path.join(OUTPATH, '*'))
+for f in files:
+    os.remove(f)
+
+with open(os.path.join(PATH, "primary.in"), 'r') as f:
+    primary_in = f.read()
+    kwargs["PRIMARYIN"] = primary_in
+with open(os.path.join(PATH, "secondary.in"), 'r') as f:
+    secondary_in = f.read()
+    kwargs["SECONDARYIN"] = secondary_in
+with open(os.path.join(PATH, "vpl.in"), 'r') as f:
+    vpl_in = f.read()
+    kwargs["VPLIN"] = vpl_in
+
+
+# ===================================================
+# Run simulation
+# ===================================================
+
+theta = np.array([ 1.07753052,  1.06771651,  4.37292767,  7.77321418, -1.46275137,
+        0.98559053,  6.5468505 ,  0.15460172,  2.16161671])
+
+# theta = [1.0768364142412223,
+# 		 1.0660648780006898,
+# 		 6.173980935724222,
+# 		 7.108016357938551,
+# 		 0.4648496167528213,
+# 		 -0.4638790248463207,
+# 		 7.160446192432624,
+# 		 0.2863339037662828,
+# 		 2.0136783756544827]
+
+kwargs['remove'] = False
+kwargs['dOutputTime'] = 1e6
+lnP = config.LnProb(theta, **kwargs)
+
+# Time -Radius Temperature -Luminosity -RotPer -LostAngMom -SurfEnFluxTotal
+pri = np.genfromtxt(glob.glob(os.path.join(OUTPATH, "*.primary.forward"))[0])
+
+# Time -Radius Temperature -Luminosity -TotEn -TotAngMom -Semim -RotPer Ecce -OrbPer -SurfEnFluxTotal
+sec = np.genfromtxt(glob.glob(os.path.join(OUTPATH, "*.secondary.forward"))[0])
+
+output = vpl.GetOutput(OUTPATH)
+
+# ===================================================
+
+YRSEC = 3600 * 24 * 365
+pri_tlock = float(output.log.final.primary.LockTime) / YRSEC
+sec_tlock = float(output.log.final.secondary.LockTime) / YRSEC
+
+tpms1 = 6e7 * (m1)**-2.5
+tpms2 = 6e7 * (m2)**-2.5
+
+pms_ind1 = np.argmin(np.abs(pri.T[0] - tpms1))
+pms_ind2 = np.argmin(np.abs(sec.T[0] - tpms2))
+
+
+summary =  "Input parameters: \n" 
+summary += r"M$_1$ = %.3f M$_{\odot}$"%(theta[0])
+summary += "\n"
+summary += r"M$_2$ = %.3f M$_{\odot}$"%(theta[1])
+summary += "\n"
+summary += r"P$_{\rm rot1}$ = %.3f d"%(theta[2])
+summary += "\n"
+summary += r"P$_{\rm rot2}$ = %.3f d"%(theta[3])
+summary += "\n"
+summary += r"$\log\tau_1$ = %.3f"%(theta[4])
+summary += "\n"
+summary += r"$\log\tau_2$ = %.3f"%(theta[5])
+summary += "\n"
+summary += r"P$_{\rm orb}$ = %.3f d"%(theta[6])
+summary += "\n"
+summary += "e = %.3f"%(theta[7])
+summary += "\n"
+summary += "age = %.3f Gyr"%(theta[8])
+
+output  = "Output: \n"
+output += r"$\ln$P = %.3f"%(lnP)
+
+
+# ===================================================
+# Plot
+# ===================================================
+
+lbls = [[r'$P_{\rm rot}$ [d]', r'Radius [$R_{\odot}$]'], 
+        [r'$P_{\rm orb}$ [d]', r'$T_{\rm eff}$ [K]'], 
+        [r'$e$', r'$L$ [$L_{\odot}$]']]
+nplots = len(lbls)
+
+lw = 1.5 
+
+fig, axs = plt.subplots(3, 2, figsize=[18, 15], sharex=True)
+plt.subplots_adjust(hspace=0.1, right=.8)
+
+for i in range(3):
+    for j in range(2):
+        axs[i][j].set_ylabel(lbls[i][j], fontsize=20)
+        axs[i][j].axvline(pri_tlock, color='b', linestyle='--')
+        axs[i][j].axvline(sec_tlock, color='r', linestyle='--')
+        
+# Prot
+axs[0][0].plot(pri.T[0], pri.T[4], color='b', linewidth=lw, label='Pri')
+axs[0][0].plot(sec.T[0], sec.T[7], color='r', linewidth=lw, label='Sec')
+axs[0][0].axhline(prot1, color='m', linestyle='--')
+axs[0][0].fill_between(pri.T[0], y1=prot1-prot1Sig, y2=prot1+prot1Sig, color='m', alpha=.05)
+axs[0][0].scatter(pri.T[0][pms_ind1], pri.T[4][pms_ind1], color='b')
+axs[0][0].scatter(sec.T[0][pms_ind2], sec.T[7][pms_ind2], color='r')
+
+# Radius
+axs[0][1].plot(pri.T[0], pri.T[1], linewidth=lw, color='b')
+axs[0][1].plot(sec.T[0], sec.T[1], linewidth=lw, color='r')
+axs[0][1].axhline(r1, color='b', linestyle='--')
+axs[0][1].axhline(r2, color='r', linestyle='--')
+axs[0][1].fill_between(pri.T[0], y1=r1-r1Sig, y2=r1+r1Sig, color='b', alpha=.05)
+axs[0][1].fill_between(pri.T[0], y1=r2-r2Sig, y2=r2+r2Sig, color='r', alpha=.05)
+axs[0][1].scatter(pri.T[0][pms_ind1], pri.T[1][pms_ind1], color='b')
+axs[0][1].scatter(sec.T[0][pms_ind2], sec.T[1][pms_ind2], color='r')
+
+# Porb
+axs[1][0].plot(sec.T[0], sec.T[9], linewidth=lw, color='m')
+axs[1][0].axhline(porb, color='m', linestyle='--')
+axs[1][0].fill_between(pri.T[0], y1=porb-porbSig, y2=porb+porbSig, color='m', alpha=.05)
+axs[1][0].scatter(sec.T[0][pms_ind2], sec.T[9][pms_ind2], color='m')
+
+# Teff
+axs[1][1].plot(pri.T[0], pri.T[2], linewidth=lw, color='b')
+axs[1][1].plot(sec.T[0], sec.T[2], linewidth=lw, color='r')
+axs[1][1].axhline(teff1, color='b', linestyle='--')
+axs[1][1].axhline(teff2, color='r', linestyle='--')
+axs[1][1].fill_between(pri.T[0], y1=teff1-teff1Sig, y2=teff1+teff1Sig, color='b', alpha=.05)
+axs[1][1].fill_between(pri.T[0], y1=teff2-teff2Sig, y2=teff2+teff2Sig, color='r', alpha=.05)
+axs[1][1].scatter(pri.T[0][pms_ind1], pri.T[2][pms_ind1], color='b')
+axs[1][1].scatter(sec.T[0][pms_ind2], sec.T[2][pms_ind2], color='r')
+
+# ecc
+axs[2][0].plot(sec.T[0], sec.T[8], linewidth=lw, color='m')
+axs[2][0].axhline(ecc, color='m', linestyle='--')
+axs[2][0].fill_between(pri.T[0], y1=ecc-eccSig, y2=ecc+eccSig, color='m', alpha=.05)
+axs[2][0].scatter(sec.T[0][pms_ind2], sec.T[8][pms_ind2], color='m')
+
+# Luminosity
+axs[2][1].plot(pri.T[0], pri.T[3], linewidth=lw, color='b')
+axs[2][1].plot(sec.T[0], sec.T[3], linewidth=lw, color='r')
+axs[2][1].axhline(lum1, color='b', linestyle='--')
+axs[2][1].axhline(lum2, color='r', linestyle='--')
+axs[2][1].fill_between(pri.T[0], y1=lum1-lum1Sig, y2=lum1+lum1Sig, color='b', alpha=.05)
+axs[2][1].fill_between(pri.T[0], y1=lum2-lum2Sig, y2=lum2+lum2Sig, color='r', alpha=.05)
+axs[2][1].scatter(pri.T[0][pms_ind1], pri.T[3][pms_ind1], color='b')
+axs[2][1].scatter(sec.T[0][pms_ind2], sec.T[3][pms_ind2], color='r')
+
+plt.gcf().text(.82, .9, summary, fontsize=22, va='top')
+plt.gcf().text(.82, .6, output, fontsize=22, va='top')
+
+axs[2][0].set_xlabel('Time [yr]', fontsize=20)
+axs[2][1].set_xlabel('Time [yr]', fontsize=20)
+axs[0][0].legend(loc='upper left')
+plt.xscale('log')
+plt.xlim(min(sec.T[0]), max(sec.T[0]))
+plt.minorticks_on()
+# plt.tight_layout()
+plt.savefig(plotName)
+plt.show()
