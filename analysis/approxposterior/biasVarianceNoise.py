c5527c698527be73615e33fe643a50eb7820e030
jbirky <jbirky@uw.edu>
2020-11-12 17:49:41 -0800

    mse varying white noise

--- analysis/approxposterior/biasVarianceNoise.py
+++ analysis/approxposterior/biasVarianceNoise.py
@@ -39,16 +39,16 @@ fitAmp = False
 nErrSamples = 10
 nTrain = 300
 nSamp  = 400
-testScales = np.arange(-11,16,.5)
+testNoise = np.arange(-20,0,1)
 computeSamples = True
 
-simsFile = INPATH + 'apRunAPFModelCache.npz' #'simsCache.npz'
+simsFile = INPATH + 'apRunAPFModelCache.npz' 
 gpFile   = INPATH + 'apRunAPGP.npz'
-mseFile  = INPATH + 'mse_samples.npz'
+mseFile  = INPATH + 'mse_samples_noise.npz'
 
 # load optimized GP parameters, last iteration
 gpSaved = np.load(gpFile)['gpParamValues'][-1]
-white_noise = -15
+# white_noise = -15
 
 
 # ============================================
@@ -75,7 +75,7 @@ def loadSamples(nTrain=nTrain, simsFile=simsFile):
 
 # ============================================
 # Compute train and test errors
-# for varying GP scale lengths
+# for varying GP white noise values
 # ============================================
 
 if computeSamples == True:
@@ -90,25 +90,25 @@ if computeSamples == True:
 
 		tTrain, yTrain, tTest, yTest = loadSamples(nTrain=nTrain, simsFile=simsFile)
 
-		gp = gpUtils.defaultGP(tTrain, yTrain, white_noise=white_noise, fitAmp=fitAmp)
-		gp.compute(tTrain)
-
-		gpPar = np.zeros(gp.get_parameter_vector().shape[0])
-
 		trainErr = []
 		testErr  = []
 
-		for scale in tqdm.tqdm(testScales):
-			if fitAmp == True:
-				gpPar[0] = np.mean(yTrain)
-				gpPar[1] = 10 #np.var(yTrain)
-				gpPar[2:] = scale
-			else:
-				gpPar[0] = np.mean(yTrain)
-				gpPar[1:] = scale
+		for noise in tqdm.tqdm(testNoise):
+			gp = gpUtils.defaultGP(tTrain, yTrain, white_noise=noise, fitAmp=fitAmp)
+			if gpSaved is not None:
+				gp.set_parameter_vector(gpSaved)
+			gp.compute(tTrain)
+		
+			# if fitAmp == True:
+			# 	gpPar[0] = np.mean(yTrain)
+			# 	gpPar[1] = np.var(yTrain)
+			# 	gpPar[2:] = scale
+			# else:
+			# 	gpPar[0] = np.mean(yTrain)
+			# 	gpPar[1:] = scale
 
-			gp.set_parameter_vector(gpPar)
-			gp.recompute()
+			# gp.set_parameter_vector(gpPar)
+			# gp.recompute()
 
 			yTrainPred = gp.predict(yTrain, tTrain)[0]
 			mseTrain = mse(yTrain, yTrainPred)
@@ -121,49 +121,35 @@ if computeSamples == True:
 		trainErrSamples.append(trainErr)
 		testErrSamples.append(testErr)
 
-		# Compute train/test error for optimized gp scale parameters
-		if gpSaved is not None:
-
-			gp.set_parameter_vector(gpSaved)
-			gp.recompute()
-
-			yTrainOpt = gp.predict(yTrain, tTrain)[0]
-			mseTrainOpt = mse(yTrain, yTrainOpt)
-			trainErrOpt.append(mseTrainOpt)
-
-			yTestOpt = gp.predict(yTrain, tTest)[0]
-			mseTestOpt = mse(yTest, yTestOpt)
-			testErrOpt.append(mseTestOpt)
-
-	np.savez(mseFile, testScales=testScales, trainErrSamples=trainErrSamples, \
-		testErrSamples=testErrSamples, trainErrOpt=trainErrOpt, testErrOpt=testErrOpt)
+	np.savez(mseFile, testNoise=testNoise, trainErrSamples=trainErrSamples, \
+		testErrSamples=testErrSamples)
 
 
 # ============================================
 # Load results
 # ============================================
 
-tTrain, yTrain, tTest, yTest = loadSamples(nTrain=nTrain, simsFile=simsFile)
+# tTrain, yTrain, tTest, yTest = loadSamples(nTrain=nTrain, simsFile=simsFile)
 
-gp = gpUtils.defaultGP(tTrain, yTrain, white_noise=white_noise, fitAmp=fitAmp)
-gp.compute(tTrain)
+# gp = gpUtils.defaultGP(tTrain, yTrain, white_noise=white_noise, fitAmp=fitAmp)
+# gp.compute(tTrain)
 
-gp.set_parameter_vector(gpSaved)
-gp.recompute()
+# gp.set_parameter_vector(gpSaved)
+# gp.recompute()
 
-yTrainOpt = gp.predict(yTrain, tTrain)[0]
-mseTrainOpt = mse(yTrain, yTrainOpt)
+# yTrainOpt = gp.predict(yTrain, tTrain)[0]
+# mseTrainOpt = mse(yTrain, yTrainOpt)
 
-yTestOpt = gp.predict(yTrain, tTest)[0]
-mseTestOpt = mse(yTest, yTestOpt)
+# yTestOpt = gp.predict(yTrain, tTest)[0]
+# mseTestOpt = mse(yTest, yTestOpt)
 
-print('Opt GP train error: %.2e'%(mseTrainOpt))
-print('Opt GP test error: %.2e'%(mseTestOpt))
+# print('Opt GP train error: %.2e'%(mseTrainOpt))
+# print('Opt GP test error: %.2e'%(mseTestOpt))
 
-mse_samples = np.load(mseFile)
-testScales = mse_samples['testScales']
-trainErrSamples = mse_samples['trainErrSamples']
-testErrSamples = mse_samples['testErrSamples']
+# mse_samples = np.load(mseFile)
+# testScales = mse_samples['testScales']
+# trainErrSamples = mse_samples['trainErrSamples']
+# testErrSamples = mse_samples['testErrSamples']
 
 
 # ============================================
@@ -173,23 +159,23 @@ testErrSamples = mse_samples['testErrSamples']
 fig, axs = plt.subplots(2, 1, figsize=[10,12], sharex=True)
 
 for j in range(nErrSamples):
-	axs[0].plot(testScales, testErrSamples[j], color='g', linewidth=1, alpha=.1)
-	axs[1].plot(testScales, trainErrSamples[j], color='b', linewidth=1, alpha=.1)
-axs[0].plot(testScales, np.mean(testErrSamples, axis=0), label='Test error (N=%s)'%(nSamp - nTrain), color='g', linewidth=2)
-axs[1].plot(testScales, np.mean(trainErrSamples, axis=0), label='Train error (N=%s)'%(nTrain), color='b', linewidth=2)
+	axs[0].plot(testNoise, testErrSamples[j], color='g', linewidth=1, alpha=.1)
+	axs[1].plot(testNoise, trainErrSamples[j], color='b', linewidth=1, alpha=.1)
+axs[0].plot(testNoise, np.mean(testErrSamples, axis=0), label='Test error (N=%s)'%(nSamp - nTrain), color='g', linewidth=2)
+axs[1].plot(testNoise, np.mean(trainErrSamples, axis=0), label='Train error (N=%s)'%(nTrain), color='b', linewidth=2)
 
-if gpSaved is not None:
+# if gpSaved is not None:
 	# cmap = mpl.cm.Spectral
 	# nscales = len(gpSaved) - 1
 	# colors = [cmap(i/nscales) for i in range(nscales)]
 	# random.shuffle(colors)
 
-	for i in range(1, len(gpSaved)):
-		axs[0].axvline(gpSaved[i], color='r', linewidth=.5, linestyle='--')
-		axs[1].axvline(gpSaved[i], color='r', linewidth=.5, linestyle='--')
+	# for i in range(1, len(gpSaved)):
+	# 	axs[0].axvline(gpSaved[i], color='r', linewidth=.5, linestyle='--')
+	# 	axs[1].axvline(gpSaved[i], color='r', linewidth=.5, linestyle='--')
 
-	axs[0].axhline(mseTestOpt, color='r', linestyle='--', label='Opt GP test error: %.2e'%(mseTestOpt))
-	axs[1].axhline(mseTrainOpt, color='r', linestyle='--', label='Opt GP train error: %.2e'%(mseTrainOpt))
+	# axs[0].axhline(mseTestOpt, color='r', linestyle='--', label='Opt GP test error: %.2e'%(mseTestOpt))
+	# axs[1].axhline(mseTrainOpt, color='r', linestyle='--', label='Opt GP train error: %.2e'%(mseTrainOpt))
 
 axs[0].axhline(np.var(yTest), color='k', linestyle='--', label='var(yTest)')
 axs[1].axhline(np.var(yTrain), color='k', linestyle='--', label='var(yTrain)')
@@ -200,13 +186,13 @@ axs[0].set_yscale('log')
 axs[1].set_yscale('log')
 axs[0].set_ylabel(r'MSE', fontsize=20)
 axs[1].set_ylabel(r'MSE', fontsize=20)
-plt.xlabel(r'GP log scale parameter', fontsize=20)
+plt.xlabel(r'GP log noise parameter', fontsize=20)
 axs[0].set_title('CTL model', fontsize=25)
-plt.xlim(max(testScales), min(testScales))
+plt.xlim(max(testNoise), min(testNoise))
 plt.minorticks_on()
 plt.tight_layout()
 if fitAmp == True:
-	plt.savefig(os.path.join(plotDir, 'gp_bias_variance_amp.png'))
+	plt.savefig(os.path.join(plotDir, 'gp_bias_variance_noise_amp.png'))
 else:
-	plt.savefig(os.path.join(plotDir, 'gp_bias_variance_noamp.png'))
+	plt.savefig(os.path.join(plotDir, 'gp_bias_variance_noise_noamp.png'))
 plt.close()
